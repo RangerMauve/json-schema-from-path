48f1ed000804e05573b21a26554863c9fb85625d
Howard Tang <aychtang@gmail.com>
2019-09-03 16:13:36 +0200

    Add test for oneOf schema case

--- test.js
+++ test.js
@@ -15,9 +15,9 @@ var tests = [{
 	path: "foo/0/bar",
 	realPath: ["properties", "foo", "items", "properties", "bar"]
 }, {
-    schema: {properties: {foo: {items: {properties: { bar: {type: "number"}}}}}},
-    path: "foo.0.bar",
-    realPath: ["properties", "foo", "items", "properties", "bar"]
+	schema: {properties: {foo: {items: {properties: { bar: {type: "number"}}}}}},
+	path: "foo.0.bar",
+	realPath: ["properties", "foo", "items", "properties", "bar"]
 }, {
 	schema: {additionalProperties: {type: "string"}},
 	path: "foo",
@@ -27,9 +27,9 @@ var tests = [{
 	path: "foo",
 	realPath: ["patternProperties", ".*"]
 }, {
-		schema: { properties: { foo: { type: "string" } } },
-		path: "/foo/",
-		realPath: ["properties", "foo"]
+	schema: { properties: { foo: { type: "string" } } },
+	path: "/foo/",
+	realPath: ["properties", "foo"]
 }]
 
 tests.forEach(function(test, index) {
@@ -42,6 +42,28 @@ tests.forEach(function(test, index) {
 		"Failed test " + index
 	);
 });
+
+var oneOfSchema = {
+	properties: {
+		example: {
+			oneOf: [{
+				title: 'target',
+				type: 'string',
+				value: 'hello world'
+			}]
+		}
+	}
+};
+
+assert.deepStrictEqual(
+	getSchemaFromPath(oneOfSchema, 'example.target'),
+	{
+		title: 'target',
+		type: 'string',
+		value: 'hello world'
+	},
+	"Failed test for oneOf schemas"
+);
 
 function pluck(object, path) {
 	var clone = path.slice();
